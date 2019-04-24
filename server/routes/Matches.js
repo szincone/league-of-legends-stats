3b2c41cd89c47f182f76649154b714562f5fff8e
szincone <sawyerzincone@gmail.com>
2019-04-24 15:01:47 -0600

    got first summon api call working

--- server/routes/Matches.js
+++ server/routes/Matches.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 const responseStatus = {
   success: 200,
@@ -8,7 +9,21 @@ const responseStatus = {
   notAcceptable: 406,
   internalErr: 500,
 };
+const baseURL = 'https://na1.api.riotgames.com/lol/';
 
+router.get('/:summoner_name', async (req, res) => {
+  try {
+    const summonerName = req.params.summoner_name;
+    const summonerInfo = await axios.get(
+      `${baseURL}summoner/v4/summoners/by-name/${summonerName}?api_key=${
+        process.env.API_KEY
+      }`,
+    );
+    res.status(responseStatus.success).json(summonerInfo.data);
+  } catch (err) {
+    res.status(responseStatus.internalErr).json(err);
+  }
+});
 router.get('/', async (req, res) => {
   try {
     res
