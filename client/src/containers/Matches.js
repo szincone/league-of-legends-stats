764bd996f438278a7b09882bfdaa37a4bef01f7b
szincone <sawyerzincone@gmail.com>
2019-04-24 14:38:56 -0600

    done w/ basic styling

--- client/src/containers/Matches.js
+++ client/src/containers/Matches.js
@@ -2,55 +2,98 @@ import React from 'react';
 
 function Matches(props) {
   return (
-    <>
+    <div
+      style={{
+        background: 'green',
+        width: '80%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       {props.matches.map((match) => (
-        <div
-          key={match.id}
-          style={{
-            background: 'red',
-            margin: '1rem',
-            padding: '1rem',
-            width: '80%',
-          }}
-        >
-          {/* start game type div */}
-          <div>
-            {'game'}
-            {'lorem ipsum text'}
+        <>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <h5>{'Ranked Solo'}</h5>
+            <h5>{'19 minutes ago'}</h5>
+            <h5>{'____'}</h5>
+            <h5>{'Victory'}</h5>
+            <h5>{'32m 17s'}</h5>
           </div>
           {/* start user type div */}
-          <div>
-            {'user'}
-            {'lorem ipsum text'}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                {'user_img'}
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <h5>{'spell1'}</h5>
+                <h5>{'spell2'}</h5>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  margin: '0 2px',
+                }}
+              >
+                <h5>{'rune1'}</h5>
+                <h5>{'rune2'}</h5>
+              </div>
+            </div>
+            {'champion name'}
           </div>
           {/* start KDA type div */}
           <div>
-            {'KDA'}
-            {'lorem ipsum text'}
+            <h5>{'Kill/Death/Assist'}</h5>
           </div>
           {/* start level type div */}
           <div>
-            {'level'}
-            {'lorem ipsum text'}
+            <h6>{'champion level'}</h6>
+            <h6>{'209 (8.1) creep score'}</h6>
           </div>
           {/* start items type div */}
           <div>
-            {'items'}
-            {'lorem ipsum text'}
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}
+            >
+              {'map over items here'}
+            </div>
           </div>
           {/* start participants type div */}
           <div>
-            {'participarnts'}
-            {'lorem ipsum text'}
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              {'map over particpants here'}
+            </div>
           </div>
-          {/* dropdown button */}
-          <button>
-            {'button'}
-            {'lorem ipsum text'}
-          </button>
-        </div>
+          <button>&#x2193;</button>
+        </>
       ))}
-    </>
+    </div>
   );
 }
 
