a4dade0b3e91576ee07dcac10192772ff6cff3d9
szincone <sawyerzincone@gmail.com>
2019-04-24 12:44:46 -0600

    added summoner form to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Matches from './containers/Matches';
+import SummonerForm from './containers/SummonerForm';
 
 function App() {
   const [matches, setMatches] = useState([]);
+  const [summonerName, setSummonerName] = useState('');
 
   const updateMatches = async () => {
     const response = [{ id: 0, message: 'Hey' }, { id: 1, message: 'Hola' }];
@@ -14,12 +16,30 @@ function App() {
     updateMatches();
   }, []);
 
+  const inputChangeHandler = (e) => {
+    const { value } = e.target;
+    setSummonerName(value);
+  };
+
+  console.log('SUMM NAME', summonerName);
+
   return (
     <div>
       <Route
         exact
         path="/"
         render={(props) => <Matches {...props} matches={matches} />}
+      />
+      <Route
+        exact
+        path="/"
+        render={(props) => (
+          <SummonerForm
+            {...props}
+            summonerName={summonerName}
+            inputChangeHandler={inputChangeHandler}
+          />
+        )}
       />
     </div>
   );
